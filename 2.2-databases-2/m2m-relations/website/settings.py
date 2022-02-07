388e2cc707454b4ebc284283d62fd6471d399fda
Partizan-coder <79650117+Partizan-coder@users.noreply.github.com>
2022-02-07 23:50:14 +0300

    Update settings.py
    Delete password

--- 2.2-databases-2/m2m-relations/website/settings.py
+++ 2.2-databases-2/m2m-relations/website/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'Django_ORM_2',
         'USER': "postgres",
-        'PASSWORD': "y@ppizo9",
+        'PASSWORD': "",
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
